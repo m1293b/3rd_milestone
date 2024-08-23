80dc8a7fff594c0838703e4f252e65ac9d07d454
m1293b <m1293b@gmail.com>
2024-08-23 19:02:43 +0000

    creating the base page

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello world"
+    return render_template("index.html")
 
-print("Hello")
+if __name__ == "__main__":
+    app.run(
+        host = os.environ.get("IP", "0.0.0.0"),
+        port = int(os.environ.get("PORT", "5000")),
+        debug = True #delete this line before submiting project
+    )
