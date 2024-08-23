3023eaf9edd3cbd3a06f3633a6faf582f58c2e99
m1293b <m1293b@gmail.com>
2024-08-23 19:21:52 +0000

    adding contact page, still testing

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ def index():
 def about():
     return render_template("about.html")
 
+@app.route('/contact')
+def about():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(
         host = os.environ.get("IP", "0.0.0.0"),
