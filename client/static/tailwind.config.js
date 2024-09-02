2bda42c8f154ba829d2ef65294e24878eb803cd6
m1293b <m1293b@gmail.com>
2024-09-02 15:25:31 +0100

    updating header element, README.md file

--- client/static/tailwind.config.js
+++ client/static/tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
-  purge: ['../static/**/*.{js,jsx,ts,css,tsx,html}'],
+  // purge: ['../static/**/*.{js,jsx,ts,css,tsx,html}'],
   content: ['../static/**/*.{js,jsx,ts,css,tsx,html}'],
   theme: {
+    screens: {
+    'tablet': '640px',
+    // => @media (min-width: 640px) { ... }
+
+    'laptop': '1024px',
+    // => @media (min-width: 1024px) { ... }
+
+    'desktop': '1280px',
+    // => @media (min-width: 1280px) { ... }
+    },
     extend: {},
   },
   plugins: [],
