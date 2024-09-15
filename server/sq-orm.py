3ad752185761eca6c514e12aea9020573edc3bd6
m1293b <m1293b@gmail.com>
2024-09-15 20:09:11 +0000

    updated sign up page's layout

--- server/sq-orm.py
+++ server/sq-orm.py
@@ -1,12 +1,14 @@
 from sqalchemy import (
-    create_engine, Column, Float, ForeignKey, Integer, String
+    create_engine, Column, ForeignKey, Integer, String
 )
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# executing the instructions from the "no39_recipes" database
 db = create_engine("postgresql+psycopg2:///no39_recipes")
 base = declerative_base()
 
+# create a class-based model for the "users" table
 class Users(base):
     __tablename__ = "users"
     Column("userid", users_userid_seq, primary_key=True, server_default=users_userid_seq.next_value()),
@@ -17,9 +19,12 @@ class Users(base):
     Column("points", Integer),
     Column("liked_recipes", Integer)
 
+# instead of connecting to the database directly, we will ask for a session
+# create a new instance of sessionmaker, then point to our engine (the db)
 Session = sessionmaker(db)
-
+# opens an actual session by calling the Session() subclass defined above
 session = Session()
 
+# creating the database using declarative_base subclass
 base.metadata.create_all(db)
 
